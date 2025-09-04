d26274cc8ad6e36e1e59479719cf62e4a947f87c
AnaCarolinaFreitas <ana.c.freitas35@aluno.senai.br>
2025-09-04 15:52:05 -0300

    Feat: Server working

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require("dotenv").config(); 
 const express = require("express");
 const cors = require("cors");
+const gameRoutes = require('./src/routes/gameRoutes');
 
 const app = express();
 app.use(cors()); 
 app.use(express.json()); 
+app.use('/api', gameRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
